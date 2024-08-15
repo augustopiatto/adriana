56983b17f0bf7df06a382c022087482870742849
Augusto de Moraes Piatto <77405968+augustopiatto@users.noreply.github.com>
2024-08-15 19:09:52 -0300

    Update exercicio2.java

--- Tarefa 1/exercicio2.java
+++ Tarefa 1/exercicio2.java
@@ -1,3 +1,6 @@
+// Escreva um algoritmo para ler o número total de eleitores de um município, o número
+// de votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa
+// em relação ao total de eleitores. 
 public class Votacao {
     private int eleitores;
     private int votosBrancos;
