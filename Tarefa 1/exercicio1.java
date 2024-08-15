287e25723db9cfccfaf169d54a910f3930543d36
Augusto de Moraes Piatto <77405968+augustopiatto@users.noreply.github.com>
2024-08-15 19:09:37 -0300

    Update exercicio1.java

--- Tarefa 1/exercicio1.java
+++ Tarefa 1/exercicio1.java
@@ -1,3 +1,5 @@
+// Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva
+// a idade dessa pessoa expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias. 
 public class IdadePessoa {
     private int anos;
     private int meses;
